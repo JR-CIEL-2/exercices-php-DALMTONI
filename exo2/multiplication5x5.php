6d1eec778a7aba58a25ca360c757598f6db9d6a9
DALMTONI <dealmeida-toni@lycee-jeanrostand.fr>
2023-10-03 17:01:56 +0200

    mes exos fini ici

--- exo2/multiplication5x5.php
+++ exo2/multiplication5x5.php
@@ -1,17 +1,22 @@
 <?php
+if (isset($_GET['nb1']) && isset($_GET['nb2'])) {
+    $n = intval($_GET['nb1']);
+    $p = intval($_GET['nb2']);
+
 // Boucle pour les lignes de 1 à 5
-for ($i = 1; $i <= 5; $i++) {
-    // Boucle pour les colonnes de 1 à 5
-    for ($j = 1; $j <= 5; $j++) {
-        
-        $resultat = $i * $j;
-        
-        // La largeur totale est définie à 5 caractères
-        $str = str_pad($resultat, 5);
-        
-        echo $str;
+    for ($i = 1; $i <= $n; $i++) {
+        // Boucle pour les colonnes de 1 à 5
+        for ($j = 1; $j <= $p; $j++) {
+
+            $resultat = $i * $j;
+
+            // La largeur totale est définie à 5 caractères
+            $str = str_pad($resultat, 5, '.',STR_PAD_LEFT);
+
+            echo $str;
+        }
+
+        echo "<br>";
     }
-    
-    echo "\n";
 }
 ?>
--- exo3/factorielle.php
+++ exo3/factorielle.php
@@ -1,31 +1,33 @@
 <?php
 
-function factorielle($n) {
-    if ($n == 0) {
-        return 1; // 0 est défini comme égal à 1
-    } else {
-        $resultat = 1;
-        for ($i = 1; $i <= $n; $i++) {
-           $resultat = $resultat * $i;
+if (isset($_GET['nb1']) && isset($_GET['nb2'])) {
+    $n = intval($_GET['nb1']);
+    $m = intval($_GET['nb2']);
+
+    function factorielle($n)
+    {
+        if ($n == 0) {
+            return 1; // 0 est défini comme égal à 1
+        } else {
+            $resultat = 1;
+            for ($i = 1; $i <= $n; $i++) {
+                $resultat = $resultat * $i;
+            }
+            return $resultat;
         }
-        return $resultat;
     }
-}
 
-
-function somme($m) {
-    $total = 0;
-    for ($i = 1; $i <= $m; $i++) {
-        $total = $total + $i;
+    function somme($m)
+    {
+        $total = 0;
+        for ($i = 1; $i <= $m; $i++) {
+            $total = $total + $i;
+        }
+        return $total;
     }
-    return $total;
+
+    echo "Factorielle de $n : " . factorielle($n) . "<br>";
+    echo "Somme de 1 a $m : " . somme($m) . "<br>";
+
 }
-
-
-$n = 5;
-$m = 12;
-echo "Factorielle de $n : " . factorielle($n) . "\n"; 
-echo "Somme de 1 à $m : " . somme($m) . "\n";  
-
-
 ?>
